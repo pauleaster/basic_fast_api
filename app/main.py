3d47a49ee31842b05695a0f0314b9fd518350ef7
Paul Easter <paul.john.easter@gmail.com>
2024-03-01 17:21:59 +1100

    Updated to /django/api/contacts as the api endpoint

--- app/main.py
+++ app/main.py
@@ -18,13 +18,13 @@ def get_db():
     finally:
         db.close()
 
-@app.post("/contacts/", response_model=schemas.Contact)
+@app.post("/django/api/contacts/", response_model=schemas.Contact)
 def create_contact(contact: schemas.ContactCreate, db: Session = Depends(get_db)):
     print("Creating contact")
     print(f"Contact = \n{contact}\n")
     return crud.create_contact(db=db, contact=contact)
 
-@app.get("/contacts/{contact_id}", response_model=schemas.Contact)
+@app.get("/django/api/contacts/{contact_id}", response_model=schemas.Contact)
 def read_contact(contact_id: int, db: Session = Depends(get_db)):
     print(f"Reading contact with id = {contact_id}")
     db_contact = crud.get_contact(db, contact_id=contact_id)
@@ -32,7 +32,7 @@ def read_contact(contact_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="Contact not found")
     return db_contact
 
-@app.delete("/contacts/{contact_id}")
+@app.delete("/django/api/contacts/{contact_id}")
 def delete_contact(contact_id: int, db: Session = Depends(get_db)):
     if not crud.delete_contact(db=db, contact_id=contact_id):
         raise HTTPException(status_code=404, detail="Contact not found")
